169d7b9da631947fab097afac497413dd3052df6
Bydimitri74 <dimitrileo20020709@gmail.com>
2023-04-06 23:37:01 +0000

    Securing the script with variable type checking

--- client.lua
+++ client.lua
@@ -3,10 +3,22 @@ OpenMenu = false
 RMenu.Add('Shop', 'main', RageUI.CreateMenu("Shop", "Faites votre choix", 10, 80))
 RMenu:Get('Shop', 'main').Closed = function()
     OpenMenu = false
+    if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^2Info^7] The menu is closed") end
 end
 
 local OpenShop = function(key)
+    if OpenMenu then return end
+    if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^2Info^7] The menu is opening") end
+    if  key == nil or not assert(type(key)) == "number" then if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^1ERROR^7] (^3Key^7) does not return in number") Shop.StopPrint = false end OpenMenu = false return end
+    local KeyTable = Shop.PosShop[tonumber(key)]
+    if  KeyTable == nil or KeyTable == '{}' or not assert(type(KeyTable)) == "table" then if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^1ERROR^7] (^3KeyTable^7) does not return in table") Shop.StopPrint = false end OpenMenu = false return end
+    local GetShopType = Shop[KeyTable.Group]
+    if  GetShopType == nil or GetShopType == '{}' or not assert(type(GetShopType)) == "table" then if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^1ERROR^7] (^3GetShopType^7) does not return in table") Shop.StopPrint = false end OpenMenu = false return end
+    if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^2Info^7] The menu is open") end
+    RageUI.Visible(RMenu:Get('Shop', 'main'), true)
+    if not RageUI.Visible(RMenu:Get('Shop', 'main')) or not assert(type(RageUI.Visible(RMenu:Get('Shop', 'main')))) == "boolean" then if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^1ERROR^7] (^3RageUI.Visible^7) RageUI.Visible request failed") Shop.StopPrint = false end OpenMenu = false return else print(""..Shop.Prefix.." [^2Info^7] RageUI.Visible request completed successfully") end
 
+    
 end
 
 Citizen.CreateThread(function()
@@ -23,8 +35,9 @@ Citizen.CreateThread(function()
                     interval = 0
                     DisplayHelpText("Appuyez sur ~INPUT_CONTEXT~ pour accèder menu")
                     if IsControlJustReleased(0, 38) then
-                        if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^1Info^7] Open the menu") Shop.StopPrint = false end
+                        if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^2Info^7] To request the opening of the menu") end
                         OpenShop(k)
+                        OpenMenu = true
                     end
                 end
             end
@@ -34,7 +47,7 @@ Citizen.CreateThread(function()
 end)
 
 function DisplayHelpText(str)
-    if str == nil then if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^1ERROR^7] (^3DisplayHelpText^7) Non Défini") Shop.StopPrint = false end return end
+    if str == nil then if not Shop.Production and Shop.StopPrint then print(""..Shop.Prefix.." [^1ERROR^7] (^3DisplayHelpText^7) Not defined") Shop.StopPrint = false end OpenMenu = false return end
     SetTextComponentFormat("STRING")
     AddTextComponentString(str)
     DisplayHelpTextFromStringLabel(0, 0, 1, -1)
